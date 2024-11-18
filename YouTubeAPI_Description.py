f44d81ff10bef9167bec96a31973282039d9c581
3bdulra7manamir <ethanhunt4d@gmail.com>
2024-11-18 23:40:44 +0200

    Update

--- YouTubeAPI_Description.py
+++ YouTubeAPI_Description.py
@@ -9,7 +9,7 @@ from googleapiclient.errors import HttpError
 
 api_key = "AIzaSyC5lLd1CMhXwEBR37hSX1JWWGt_3dkb9AM"  # "My YouTube API Access KEY"
 client_secrets_file = "D:\\Projects\\PyCharm\\YouTube_API_Description\\client_secret_92936865464-f9cpbdctpcum6c32ivdi87nsfh8f7o0d.apps.googleusercontent.com.json"  # "Path to Client Auth Json file"
-api_service_name = "youtube"  # ""
+api_service_name = "youtube"  # "service name gdrive etc"
 api_version = "v3"  # "API Version"
 scopes = ["https://www.googleapis.com/auth/youtube", "https://www.googleapis.com/auth/youtube.force-ssl"]
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  # here
@@ -21,11 +21,11 @@ Shadow#1942"""]
 # "Enter the new lines that will replace the old ones"
 new_lines = ["https://discord.gg/CHZea8zvBG", ""]
 
-# "Authenticate and build the YouTube API client"   # here
+# "Authenticate and build the YouTube API client"   # You are Authorized
 flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
         client_secrets_file, scopes)
 
-creds = None   # here
+creds = None   # init Value
 if os.path.exists('token.json'):  # "Received Access Token file"
     creds = Credentials.from_authorized_user_file('token.json', scopes)  # here
 
@@ -45,9 +45,9 @@ youtube = googleapiclient.discovery.build(  # here
 
 try:
     # "Retrieve a list of all the videos in your channel"
-    channels_response = youtube.channels().list(part="contentDetails", mine=True).execute()  # ""
+    channels_response = youtube.channels().list(part="contentDetails", mine=True).execute()  # "Return Channel videos List"
     channel_id = channels_response["items"][0]["id"]  # "Requests Channel ID"
-    playlist_id = channels_response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]  # here
+    playlist_id = channels_response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]  # All videos in play list
     videos = []  # a Videos List for saving info
     next_page_token = None  # here
 
