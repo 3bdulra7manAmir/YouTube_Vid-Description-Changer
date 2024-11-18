7b64dca81c57fb61c5279b0450e644ae448126af
3bdulra7manamir <ethanhunt4d@gmail.com>
2024-11-18 23:46:09 +0200

    Enhancements

--- YouTubeAPI_Description.py
+++ YouTubeAPI_Description.py
@@ -1,75 +1,118 @@
-import os.path
+import os
 import re
-import google_auth_oauthlib
-import googleapiclient.discovery
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-api_key = "AIzaSyC5lLd1CMhXwEBR37hSX1JWWGt_3dkb9AM"  # "My YouTube API Access KEY"
-client_secrets_file = "D:\\Projects\\PyCharm\\YouTube_API_Description\\client_secret_92936865464-f9cpbdctpcum6c32ivdi87nsfh8f7o0d.apps.googleusercontent.com.json"  # "Path to Client Auth Json file"
-api_service_name = "youtube"  # "service name gdrive etc"
-api_version = "v3"  # "API Version"
-scopes = ["https://www.googleapis.com/auth/youtube", "https://www.googleapis.com/auth/youtube.force-ssl"]
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  # here
+# Configuration
+SCOPES = ["https://www.googleapis.com/auth/youtube", "https://www.googleapis.com/auth/youtube.force-ssl"]
+CLIENT_SECRETS_FILE = "path/to/client_secrets.json"  # Update this path
+TOKEN_FILE = "token.json"
 
-# "Enter the lines you want it to be replaced"
-old_lines = ["https://discord.gg/seGhsz3", """Discord Account:
-Shadow#1942"""]
+# Replace lines in descriptions
+OLD_LINES = ["https://discord.gg/seGhsz3", "Discord Account:\nShadow#1942"]
+NEW_LINES = ["https://discord.gg/CHZea8zvBG", ""]
 
-# "Enter the new lines that will replace the old ones"
-new_lines = ["https://discord.gg/CHZea8zvBG", ""]
 
-# "Authenticate and build the YouTube API client"   # You are Authorized
-flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
+def authenticate_youtube_api():
+    """
+    Authenticate and build the YouTube API client.
+    """
+    creds = None
+    if os.path.exists(TOKEN_FILE):
+        creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
 
-creds = None   # init Value
-if os.path.exists('token.json'):  # "Received Access Token file"
-    creds = Credentials.from_authorized_user_file('token.json', scopes)  # here
+    # Refresh or reauthenticate if credentials are invalid
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(TOKEN_FILE, "w") as token:
+            token.write(creds.to_json())
 
-if not creds or not creds.valid:  # "if no Access Token received Then Try Again"
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())  # "Try Again code line"
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(  # here
-            "D:\\Projects\\PyCharm\\YouTube_API_Description\\client_secret_92936865464-f9cpbdctpcum6c32ivdi87nsfh8f7o0d.apps.googleusercontent.com.json",
-            scopes)
-        creds = flow.run_local_server(port=0)  # here
-    with open('token.json', 'w') as token:  # "if Access Token Received Save it"
-        token.write(creds.to_json())
+    return build("youtube", "v3", credentials=creds)
 
-youtube = googleapiclient.discovery.build(  # here
-    api_service_name, api_version, credentials=creds)
 
-try:
-    # "Retrieve a list of all the videos in your channel"
-    channels_response = youtube.channels().list(part="contentDetails", mine=True).execute()  # "Return Channel videos List"
-    channel_id = channels_response["items"][0]["id"]  # "Requests Channel ID"
-    playlist_id = channels_response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]  # All videos in play list
-    videos = []  # a Videos List for saving info
-    next_page_token = None  # here
+def get_channel_videos(youtube):
+    """
+    Retrieve all videos from the authenticated user's channel.
+    """
+    try:
+        # Get channel details
+        channels_response = youtube.channels().list(part="contentDetails", mine=True).execute()
+        uploads_playlist_id = channels_response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
 
-    while True:
-        playlist_items_response = youtube.playlistItems().list(part="snippet", playlistId=playlist_id, maxResults=111,
-                                                               pageToken=next_page_token).execute()
-        videos += playlist_items_response["items"]
-        next_page_token = playlist_items_response.get("nextPageToken")
-        if not next_page_token:  # "if there are no more pages then get out"
-            break
+        # Retrieve all videos in the uploads playlist
+        videos = []
+        next_page_token = None
+        while True:
+            playlist_items_response = youtube.playlistItems().list(
+                part="snippet",
+                playlistId=uploads_playlist_id,
+                maxResults=50,
+                pageToken=next_page_token
+            ).execute()
+            videos.extend(playlist_items_response["items"])
+            next_page_token = playlist_items_response.get("nextPageToken")
+            if not next_page_token:
+                break
+        return videos
+    except HttpError as error:
+        print(f"An HTTP error occurred: {error}")
+        return []
 
-    # Loop through all the videos and replace the specified lines in the description
-    for video in videos:  # ""
-        video_id = video["snippet"]["resourceId"]["videoId"]  # ""
-        video_title = video["snippet"]["title"]  # ""
-        video_response = youtube.videos().list(part="snippet", id=video_id).execute()  # ""
-        description = video_response["items"][0]["snippet"]["description"]  # ""
-        for i in range(len(old_lines)):  # ""
-            description = re.sub(old_lines[i], new_lines[i], description)  # ""
-        youtube.videos().update(part="snippet",
-                                body={"id": video_id, "snippet": {"description": description, "categoryId": 20, "title": video_title}}).execute()  # ""
-        print("Updated description for video ID:", video_id)
 
-except HttpError as error:
-    print("An HTTP error %d occurred:\n%s" % (error.resp.status, error.content))
+def update_video_description(youtube, video_id, title, description):
+    """
+    Update the description of a video.
+    """
+    try:
+        youtube.videos().update(
+            part="snippet",
+            body={
+                "id": video_id,
+                "snippet": {
+                    "title": title,
+                    "description": description,
+                    "categoryId": 20
+                }
+            }
+        ).execute()
+        print(f"Updated description for video ID: {video_id}")
+    except HttpError as error:
+        print(f"Failed to update video ID {video_id}: {error}")
+
+
+def main():
+    # Authenticate and initialize the YouTube API client
+    youtube = authenticate_youtube_api()
+
+    # Get all videos from the user's channel
+    videos = get_channel_videos(youtube)
+
+    # Update the descriptions of each video
+    for video in videos:
+        video_id = video["snippet"]["resourceId"]["videoId"]
+        title = video["snippet"]["title"]
+
+        # Get the current description
+        video_details = youtube.videos().list(part="snippet", id=video_id).execute()
+        current_description = video_details["items"][0]["snippet"]["description"]
+
+        # Replace old lines with new ones
+        updated_description = current_description
+        for old_line, new_line in zip(OLD_LINES, NEW_LINES):
+            updated_description = re.sub(re.escape(old_line), new_line, updated_description)
+
+        # Only update if the description has changed
+        if current_description != updated_description:
+            update_video_description(youtube, video_id, title, updated_description)
+
+
+if __name__ == "__main__":
+    main()
